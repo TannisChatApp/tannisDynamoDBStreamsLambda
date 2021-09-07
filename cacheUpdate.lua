173b46ffa6649864c0183a26046745dfdcac5fcb
Tejas07PSK <palashsarkar0007@gmail.com>
2021-09-07 10:09:09 +0530

    Modifications

--- cacheUpdate.lua
+++ cacheUpdate.lua
@@ -1,6 +1,6 @@
 local curr_max = redis.call('get', KEYS[1])
 local res = nil
-if ((curr_max == nil) or (tonumber(ARGV[1]) > curr_max))
+if ((curr_max == false) or (tonumber(ARGV[1]) > curr_max))
 then
     res = redis.call('set', KEYS[1], tonumber(ARGV[1]))
 end
--- streamProcessor.ts
+++ streamProcessor.ts
@@ -5,46 +5,50 @@ import { RedisClient } from 'redis';
 import { readFileSync } from 'fs';
 
 export const handler = async function (event: DynamoDBStreamEvent): Promise <any> {
+    console.log(JSON.stringify(event));
     let resultOfOps: object = {};
-    await (new DocumentClient({'region': "ap-south-1"})).update({
-        'TableName': "tannisMaxSlNos",
-        'Key': {
-            'table_name': { "S": "tannisUsers" }
-        },
-        'UpdateExpression': "set max_sl_no = :sl_no",
-        'ConditionExpression': "max_sl_no < :sl_no",
-        'ExpressionAttributeValues': {
-            ':sl_no': { 'N': event.Records[0].dynamodb.NewImage.sl_no['N'] }
-        },
-        'ReturnValues': "UPDATED_NEW"
-    }).promise().then((dbUpdateOpData: UpdateItemOutput): Promise<any> => {
-        console.log(dbUpdateOpData); resultOfOps['dbUpdateOpData'] = dbUpdateOpData;
-        return (new Promise ((resolve: any, reject: any): void => {
-            let redisClientConn: RedisClient = redis.createClient({
-                'host': "tannis-redis-cache.hurzbt.0001.aps1.cache.amazonaws.com",
-                'port': 6379,
-                'connect_timeout': 30000
-            });
-            redisClientConn.on("connect", (): void => {
-                redisClientConn.eval(
-                    readFileSync('./cacheUpdate.lua', 'utf-8'),
-                    1, 'usr_curr_sl_no',
-                    event.Records[0].dynamodb.NewImage.sl_no['N'],
-                    (evalErr: Error, res: any): void => {
-                        redisClientConn.quit();
-                        if (evalErr) { reject(evalErr); }
-                        else { resolve(res); }
-                    }
-                );
-            });
-            redisClientConn.on("error", (connErr: Error): void => {
-                redisClientConn.quit();
-                reject(connErr);
-            });
-            redisClientConn.on("quit", (): void => { console.log("Connection to redis server closed !!"); });
-        })); 
-    }).then((cacheOpsOpData: any): void => { console.log(cacheOpsOpData); resultOfOps['cacheOpsOpData'] = cacheOpsOpData; })
-    .catch((err: Error): void => { console.log(err); resultOfOps['error'] = err; })
-    .finally((): void => { console.log("Promise resolved !!"); });
+    if (event.Records[0].eventName === "INSERT") {
+        await (new DocumentClient({'region': process.env.DDB_REGION})).update({
+            'TableName': process.env.DDB_TABLE_NAME,
+            'Key': {
+                'table_name': "tannisUsers"
+            },
+            'UpdateExpression': "set max_sl_no = :sl_no",
+            'ConditionExpression': "max_sl_no < :sl_no",
+            'ExpressionAttributeValues': {
+                ':sl_no': Number(event.Records[0].dynamodb.NewImage.sl_no['N'])
+            },
+            'ReturnValues': "UPDATED_NEW"
+        }).promise().then((dbUpdateOpData: UpdateItemOutput): Promise<any> => {
+            console.log(JSON.stringify(dbUpdateOpData)); resultOfOps['dbUpdateOpData'] = dbUpdateOpData;
+            return (new Promise ((resolve: any, reject: any): void => {
+                let redisClientConn: RedisClient = redis.createClient({
+                    'host': process.env.REDIS_HOST,
+                    'port': Number(process.env.REDIS_PORT),
+                    'connect_timeout': 30000
+                });
+                redisClientConn.on("connect", (): void => {
+                    redisClientConn.eval(
+                        (readFileSync('./cacheUpdate.lua', 'utf-8')).replace(/[\n\r]+/g, '\n'),
+                        1, 'usr_curr_sl_no',
+                        event.Records[0].dynamodb.NewImage.sl_no['N'],
+                        (evalErr: Error, res: any): void => {
+                            redisClientConn.quit();
+                            if (evalErr) { reject(evalErr); }
+                            else { resolve(res); }
+                        }
+                    );
+                });
+                redisClientConn.on("error", (connErr: Error): void => {
+                    redisClientConn.quit();
+                    reject(connErr);
+                });
+                redisClientConn.on("quit", (): void => { console.log("Connection to redis server closed !!"); });
+            })); 
+        }).then((cacheOpsOpData: any): void => { console.log(cacheOpsOpData); resultOfOps['cacheOpsOpData'] = cacheOpsOpData; })
+        .catch((err: Error): void => { console.log(JSON.stringify(err)); resultOfOps['error'] = err; })
+        .finally((): void => { console.log("Promise resolved !!"); });
+    }
+    console.log(JSON.stringify(resultOfOps));
     return (resultOfOps);
 };
